90888d00d941439b53c5817540c3f57045655211
Sebastian <yoo.sebus@gmail.com>
2024-02-05 18:51:12 +0100

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SET = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'c5381187-50a5-4838-a60d-504af668b5c7';
+export const MODAL_CLOSE_SEC = 2.5;
